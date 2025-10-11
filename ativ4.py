a8303f5b41e015b5e09a0dd65731dc6a3f6f988d
Camilla <camacedoalves@gmail.com>
2025-10-11 12:40:31 -0300

    Delete ativ4.py

--- ativ4.py
+++
@@ -1,8 +0,0 @@
-### 4 - Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
-
-print('----------------------------ATIVIDADE 4-------------------------------------------')
-str = "python"
-int = 60
-
-print(f"{str} {int}" )
-print('----------------------------------------------------------------------------------')
