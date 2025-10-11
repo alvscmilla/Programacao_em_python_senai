8629cda26d90a46eca0f6c503727979ae9e8045a
Camilla <camacedoalves@gmail.com>
2025-10-11 12:40:38 -0300

    Delete ativ5.py

--- ativ5.py
+++
@@ -1,8 +0,0 @@
-### 5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado.
-print('-------------------------ATIVIDADE 5---------------------------------------------')
-frase = "Nina que casa é essa?"
-
-frase2 = input('Digite uma palavra: ')
-
-print(f"{frase} {frase2}")
-print('-------------------------------------------------------------------------------')
