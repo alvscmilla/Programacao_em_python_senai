d560bcc060c8eae64a35e67d29610eb0c7dbfad2
Camilla <camacedoalves@gmail.com>
2025-10-11 12:40:08 -0300

    Delete ativ1.py

--- ativ1.py
+++
@@ -1,9 +0,0 @@
-# 1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
-
-print('----------------------------ATIVIDADE 1----------------------------------------------------')
-num = int(input('Digite um número inteiro: '))
-
-result = num ** 2
-
-print("O resultado do quadrado do número digitado corresponde a: ", result) 
-print('-------------------------------------------------------------------------------------------')
